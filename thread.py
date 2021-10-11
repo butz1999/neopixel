639c414e6100b8dea7ccb26354be580c54602203
Andi Meuli <andi.meuli@gmail.com>
2021-10-11 18:57:24 +0200

    New threading example

---
+++ thread.py
@@ -0,0 +1,29 @@
+from queue import Queue
+from threading import Thread
+import copy
+import time
+
+# A thread that produces data
+def webserver(name, outqueue):
+	while True:
+		# Produce some data
+		outqueue.put(copy.deepcopy(time.time()))
+		time.sleep(0.1)
+		
+# A thread that consumes data
+def ledhandler(name, inqueue):
+	while True:
+		# Get some data
+		data = inqueue.get()
+		# Process the data
+		print(name, time.ctime(data))
+
+queue = Queue()
+web = Thread(target = webserver, args = ("webserver", queue))
+led = Thread(target = ledhandler, args = ("ledhandler", queue))
+
+try:
+	web.start()
+	led.start()
+except:
+    print("unable to start threads")
